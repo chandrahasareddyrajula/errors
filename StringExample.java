abce1960db7ee5c9775da96993267e70b467ae2a
chandrahasa <chandrahasareddy12506@gmail.com>
2017-03-10 15:40:41 +0530

    this error messeage

---
+++ StringExample.java
@@ -0,0 +1,10 @@
+public class StringExample{  
+public static void main(String args[]){  
+String s1="java";//creating string by java string literal  
+char ch[]={'string'};  
+String s2=new String(int);//converting char array to string  
+String s3=new String(error);//creating java string by new keyword  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}}  
